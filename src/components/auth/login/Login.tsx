d0613473677ac3acffa3c4444afad88b1b2ee080
Yeong joon <bluehome8626@naver.com>
2022-10-17 01:20:27 +0900

    feat: login page 세팅

---
+++ src/components/auth/login/Login.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div``;
+
+const ImgDiv = styled.div``;
+
+const InputDiv = styled.div``;
+
+function Login() {
+  return (
+    <Wrapper>
+      <ImgDiv>
+        <img src="" alt="" />
+      </ImgDiv>
+      <InputDiv></InputDiv>
+    </Wrapper>
+  );
+}
+
+export default Login;
---
+++ src/pages/loginPage/index.tsx
@@ -0,0 +1,7 @@
+import Login from '../../components/auth/login/Login';
+
+function LoginPage() {
+  return <Login />;
+}
+
+export default LoginPage;
