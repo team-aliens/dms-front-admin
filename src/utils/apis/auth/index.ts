ebc15fd9143352ffebe66ab7c6066602405c30a5
강석현 <tjrgus0713@gmail.com>
2022-10-17 08:44:25 +0900

    (#4) feat: auth부분 api 로직 작성

---
+++ src/utils/apis/auth/index.ts
@@ -0,0 +1,54 @@
+import { AxiosResponse } from 'axios';
+import { instance } from '../axios';
+import {
+  PostEmailAuthCodeRequest,
+  LoginRequest,
+  AuthCodeType,
+} from './request';
+import { AuthResponse, EmailDuplicateCheckResponse } from './response';
+
+export const login = async (body: LoginRequest) => {
+  const { data }: AxiosResponse<Promise<AuthResponse>> = await instance.post(
+    '/auth/tokens',
+    body,
+  );
+  return data;
+};
+
+export const postEmailAuthCode = async (body: PostEmailAuthCodeRequest) => {
+  await instance.post('/auth/code', body);
+};
+
+export const checkEmailAuthCode = async (
+  email: string,
+  auth_code: string,
+  type: AuthCodeType,
+) => {
+  await instance.get(
+    `/auth/code?email=${email}&auth_code=${auth_code}&type=${type}`,
+  );
+};
+
+export const tokenRefresh = async () => {
+  const refreshToken = localStorage.getItem('refresh_token');
+  const { data }: AxiosResponse<Promise<AuthResponse>> = await instance.post(
+    '/auth/reissue',
+    null,
+    {
+      headers: {
+        'refresh-token': refreshToken,
+      },
+    },
+  );
+  return data;
+};
+
+export const verificationEmail = async (account_id: string, email: string) => {
+  await instance.get(`/auth/email?account_id=${account_id}&email=${email}`);
+};
+
+export const checkEmailDuplicate = async (account_id: string) => {
+  const { data }: AxiosResponse<Promise<EmailDuplicateCheckResponse>> =
+    await instance.get(`/auth/account-id&account_id=${account_id}`);
+  return data;
+};
