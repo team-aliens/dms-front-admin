45a6da26100129369d3dc9c245337c1ec2a28460
Yeong joon <bluehome8626@naver.com>
2022-10-17 08:38:26 +0900

    feat: 불필요한 file 삭제 및 프리티어 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { GlobalStyle } from './style/global';
+import { StyledProvider } from 'aliens-design-system-front';
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(
+  <StyledProvider>
+    <App />
+  </StyledProvider>
+);
