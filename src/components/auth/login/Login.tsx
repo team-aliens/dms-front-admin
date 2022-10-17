43cff18819d75297c50e8467cfd1fc9c44adee2e
Yeong joon <bluehome8626@naver.com>
2022-10-17 22:41:45 +0900

    feat: login page 퍼블리싱 및 index 파일 수정

--- src/components/auth/login/Login.tsx
+++ src/components/auth/login/Login.tsx
@@ -1,18 +1,115 @@
 import styled from 'styled-components';
+import { Title, Input, CheckBox, Button } from 'aliens-design-system-front';
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+`;
 
-const ImgDiv = styled.div``;
+const ImgWrapper = styled.div`
+  width: 50%;
+  box-shadow: 0px 2px 40px rgba(52, 52, 52, 0.1);
+`;
 
-const InputDiv = styled.div``;
+const InputWrapper = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TitleDiv = styled.div`
+  width: 480px;
+  .textDiv {
+    display: flex;
+    justify-content: start;
+  }
+  .Hr {
+    width: 100%;
+    height: 1px;
+    background-color: #3d8aff;
+  }
+`;
+
+const InputDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 56px;
+`;
+
+const LoginInputDiv = styled.div``;
+
+const PasswordInputDiv = styled.div`
+  padding-top: 40px;
+`;
+
+const CheckInputDiv = styled.div`
+  padding-top: 60px;
+  padding-bottom: 36px;
+`;
+
+const SubmitInputDiv = styled.div`
+  padding-bottom: 62px;
+`;
+
+const ChoiceDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  #btn {
+    cursor: pointer;
+  }
+  #bar {
+    font-size: 14px;
+    font-weight: 600;
+    padding: 0 32px;
+  }
+`;
 
 function Login() {
   return (
     <Wrapper>
-      <ImgDiv>
+      <ImgWrapper>
         <img src="" alt="" />
-      </ImgDiv>
-      <InputDiv></InputDiv>
+      </ImgWrapper>
+      <InputWrapper>
+        <TitleDiv>
+          <div className="textDiv">
+            <Title fontSize="l">로그인</Title>
+          </div>
+          <div className="Hr" />
+        </TitleDiv>
+        <InputDiv>
+          <LoginInputDiv>
+            <Input
+              label="로그인"
+              placeholder="아이디를 입력해주세요"
+              width={480}
+              type="text"
+            />
+          </LoginInputDiv>
+          <PasswordInputDiv>
+            <Input
+              label="비밀번호"
+              placeholder="비밀번호를 입력해주세요"
+              width={480}
+              type="password"
+            />
+          </PasswordInputDiv>
+          <CheckInputDiv>
+            <CheckBox disabled={false} />
+          </CheckInputDiv>
+          <SubmitInputDiv>
+            <Button type="contained" color="primary">
+              로그인
+            </Button>
+          </SubmitInputDiv>
+          <ChoiceDiv>
+            <p id="btn">아이디 찾기</p>
+            <p id="bar">|</p>
+            <p id="btn">비밀번호 변경</p>
+          </ChoiceDiv>
+        </InputDiv>
+      </InputWrapper>
     </Wrapper>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import { GlobalStyle } from './style/global';
 import { StyledProvider } from 'aliens-design-system-front';
+import App from './App';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <StyledProvider>
     <App />
-  </StyledProvider>
+  </StyledProvider>,
 );
