e3492699db93d6469b31965bbb8b9d0cb8c144f1
Yeong joon <bluehome8626@naver.com>
2022-10-19 02:02:01 +0900

    feat: router 비밀번호 변경

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { LoginPage } from './pages/loginPage';
+import { ResetPasswordPage } from './pages/resetPasswordPage';
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/reset" element={<ResetPasswordPage />} />
       </Routes>
     </BrowserRouter>
   );
