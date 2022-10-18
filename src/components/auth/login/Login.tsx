89abf7d083f4138e04dc128bb3f548e8bbb67b4f
Yeong joon <bluehome8626@naver.com>
2022-10-18 23:05:30 +0900

    feat: login page 수정

--- src/components/auth/login/Login.tsx
+++ src/components/auth/login/Login.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Input, CheckBox, Button } from 'aliens-design-system-front';
+import { Input, CheckBox, Button, Text } from 'aliens-design-system-front';
 import { useForm } from '@/hooks/useForm';
 import { TitleBox } from '../titleBox';
 
@@ -13,46 +13,46 @@ export function Login() {
     <_Wrapper>
       <TitleBox />
       <_InputWrapper>
-        <_InputDiv>
+        <Input
+          label="로그인"
+          onChange={onHandleChange}
+          placeholder="아이디를 입력해주세요"
+          width={480}
+          type="text"
+          name="id"
+          value={state.id}
+        />
+        <_PasswordInputWrapper>
           <Input
-            label="로그인"
+            label="비밀번호"
             onChange={onHandleChange}
-            placeholder="아이디를 입력해주세요"
+            placeholder="비밀번호를 입력해주세요"
             width={480}
-            type="text"
-            name="id"
-            value={state.id}
+            type="password"
+            name="password"
+            value={state.password}
           />
-          <_PasswordInputWrapper>
-            <Input
-              label="비밀번호"
-              onChange={onHandleChange}
-              placeholder="비밀번호를 입력해주세요"
-              width={480}
-              type="password"
-              name="password"
-              value={state.password}
-            />
-          </_PasswordInputWrapper>
-          <_CheckInputWrapper>
-            <CheckBox disabled={false} label="아이디 저장" />
-          </_CheckInputWrapper>
-          <_SubmitInputWrapper>
-            <Button
-              type="contained"
-              disabled={state.password === ''}
-              color="primary"
-              size="medium"
-            >
-              로그인
-            </Button>
-          </_SubmitInputWrapper>
-          <_ChoiceWrapper>
-            <p id="btn">아이디 찾기</p>
-            <p id="bar">|</p>
-            <p id="btn">비밀번호 변경</p>
-          </_ChoiceWrapper>
-        </_InputDiv>
+        </_PasswordInputWrapper>
+        <_CheckInputWrapper>
+          <CheckBox disabled={false} label="아이디 저장" />
+        </_CheckInputWrapper>
+        <_SubmitInputWrapper>
+          <Button
+            type="contained"
+            disabled={state.password === ''}
+            color="primary"
+            size="medium"
+          >
+            로그인
+          </Button>
+        </_SubmitInputWrapper>
+        <_FindAccontWrapper>
+          <_FindAccountArea>
+            <Text cursor="pointer">아이디 찾기</Text>
+            <Text>|</Text>
+            <Text cursor="pointer">비밀번호 변경</Text>
+          </_FindAccountArea>
+        </_FindAccontWrapper>
       </_InputWrapper>
     </_Wrapper>
   );
@@ -67,14 +67,6 @@ const _Wrapper = styled.div`
 `;
 
 const _InputWrapper = styled.div`
-  width: 50%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const _InputDiv = styled.div`
   display: flex;
   flex-direction: column;
   padding-top: 56px;
@@ -93,15 +85,20 @@ const _SubmitInputWrapper = styled.div`
   padding-bottom: 62px;
 `;
 
-const _ChoiceWrapper = styled.div`
+const _FindAccontWrapper = styled.div`
   display: flex;
   justify-content: center;
+`;
+
+const _FindAccountArea = styled.div`
+  width: 238px;
+  display: flex;
+  justify-content: space-between;
   #btn {
     cursor: pointer;
   }
   #bar {
     font-size: 14px;
     font-weight: 600;
-    padding: 0 32px;
   }
 `;
