91cf4a13fea1520ddd0fdf2f02476f555c562a6f
Yeong joon <bluehome8626@naver.com>
2022-10-19 01:01:54 +0900

    feat: findId page 추가

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { LoginPage } from './pages/loginPage';
+import { FindIdPage } from './pages/findIdPage';
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/findid" element={<FindIdPage />} />
       </Routes>
     </BrowserRouter>
   );
