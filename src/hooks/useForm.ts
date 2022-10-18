d577ab7ea532fd945b68e06b57a424c2d2c3d6d9
Yeong joon <bluehome8626@naver.com>
2022-10-18 22:16:31 +0900

    feat: useForm hooks 추가

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,18 @@
+import { ChangeEvent, useState } from 'react';
+
+export const useForm = <T>(initalState: T) => {
+  const [state, setState] = useState<T>(initalState);
+  const onHandleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    const { value, name } = e.target;
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+  return {
+    onHandleChange,
+    state,
+  };
+};
