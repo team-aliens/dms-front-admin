61d85808479ffa1a018f29df541d2429ddfc5c00
강석현 <tjrgus0713@gmail.com>
2022-10-20 03:45:15 +0900

    (#3) feat: errormessage 초기값 설정해주기

--- src/hooks/useErrorMessage.tsx
+++ src/hooks/useErrorMessage.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useErrorMessage = <T extends string>(errorTypes: readonly T[]) => {
   type ErrorState = {
     [keys in typeof errorTypes[number]]: string;
   };
   const [errorMessages, setErrorMessages] = useState<ErrorState>();
+  useEffect(() => {
+    errorTypes.forEach((i) => {
+      setErrorMessages({
+        ...errorMessages,
+        [i]: '',
+      });
+    });
+  }, [errorTypes]);
   const changeErrorMessage = (errorType: keyof ErrorState, message: string) => {
     setErrorMessages({
       ...errorMessages,
