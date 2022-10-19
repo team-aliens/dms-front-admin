3b749c408bc4ac39a9a7a4a22bad0a9d4221d80b
강석현 <75576448+paperdeer@users.noreply.github.com>
2022-10-19 17:20:54 +0900

    (#10) merge: 학교 목록 불러오기 api, 파일 업로드 api 추가 (#11)
    * (#10) feat: 학교 리스트 불러오는 api 추가
    
    * (#10) feat: 파일 업로드 api 추가

--- src/apis/schools/response.ts
+++ src/apis/schools/response.ts
@@ -1,3 +1,13 @@
+interface SchoolInformation {
+  id: string;
+  name: string;
+  address: string;
+}
+
+export interface SchoolListResponse {
+  schools: SchoolInformation[];
+}
+
 export interface SchoolQuestionResponse {
   question: string;
 }
