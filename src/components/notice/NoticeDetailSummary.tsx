c7efb4b47dc981d42f353bce270a9123ec1e7d4a
강석현 <tjrgus0713@gmail.com>
2022-11-02 23:20:18 +0900

    (#29) feat: 공지사항 상세보기 api 연결

--- src/components/notice/NoticeDetailSummary.tsx
+++ src/components/notice/NoticeDetailSummary.tsx
@@ -1,16 +1,28 @@
 import { Text, Button } from 'aliens-design-system-front';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { deleteNotice } from '@/apis/notice';
 
-export const NoticeDetailSummary = () => {
+interface Props {
+  noticeId: string;
+  createdDate: Date;
+}
+
+export const NoticeDetailSummary = ({ noticeId, createdDate }: Props) => {
+  const onClickDeleteNotice = () => {
+    deleteNotice(noticeId);
+  };
   return (
     <_Wrapper>
       <Text fontSize="m" color="gray5" display="inline-block">
-        2022/10/31
+        {createdDate.toLocaleDateString()}
       </Text>
-      <Button type="outline" onClick={() => {}} color="primary">
-        수정하기
-      </Button>
-      <Button type="outline" onClick={() => {}} color="error">
+      <Link to={`/notice/write/${noticeId}`}>
+        <Button type="outline" onClick={() => {}} color="primary">
+          수정하기
+        </Button>
+      </Link>
+      <Button type="outline" onClick={onClickDeleteNotice} color="error">
         삭제하기
       </Button>
     </_Wrapper>
@@ -24,10 +36,10 @@ const _Wrapper = styled.div`
   padding-bottom: 24px;
   border-bottom: 1px solid ${({ theme }) => theme.color.gray3};
 
-  > button {
+  > a {
     margin-left: auto;
-    :last-of-type {
-      margin-left: 10px;
-    }
+  }
+  button {
+    margin-left: 10px;
   }
 `;
--- src/pages/notice/Detail.tsx
+++ src/pages/notice/Detail.tsx
@@ -3,23 +3,33 @@ import { WithNavigatorBar } from '@/components/WithNavigatorBar';
 import styled from 'styled-components';
 import { Title, Text } from 'aliens-design-system-front';
 import { NoticeDetailSummary } from '@/components/notice/NoticeDetailSummary';
+import { useEffect, useState } from 'react';
+import { NoticeDetailResponse } from '@/apis/notice/response';
+import { getNoticeDetail } from '@/apis/notice';
 
 export const NoticeDetail = () => {
-  // const { noticeId } = useParams();
+  const [detail, setDetail] = useState<NoticeDetailResponse>({
+    title: '',
+    content: '',
+    create_at: new Date(),
+  });
+  const { noticeId } = useParams();
+  useEffect(() => {
+    getNoticeDetail(noticeId).then((res) => setDetail(res));
+  }, []);
   return (
     <WithNavigatorBar>
       <_Wrapper>
         <_Path />
         <_Title fontSize="l" color="gray9" display="inline-block">
-          공지 제목이 나옵니다 공지 제목이 나옵니다
+          {detail.title}
         </_Title>
-        <NoticeDetailSummary />
+        <NoticeDetailSummary
+          createdDate={detail.create_at}
+          noticeId={noticeId}
+        />
         <_Content color="gray7" fontSize="m" display="inline-block">
-          공지 내용이 나옵니다 공지 내용이 나옵니다 공지 내용이 나옵니다 공지
-          내용이 나옵니다 공지 내용이 나옵니다 공지 내용이 나옵니다 공지 내용이
-          나옵니다 공지 내용이 나옵니다 공지 내용이 나옵니다 공지 내용이
-          나옵니다 공지 내용이 나옵니다 공지 내용이 나옵니다 공지 내용이
-          나옵니다 공지 내용이 나옵니다 공지 내용이 나옵니다
+          {detail.content}
         </_Content>
       </_Wrapper>
     </WithNavigatorBar>
