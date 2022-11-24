cc2ebce938547774dd1bb791d8ce6b6eac63b721
강석현 <tjrgus0713@gmail.com>
2022-11-24 11:42:30 +0900

    (#34) feat: react-query 다운 및 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,24 @@
 import { StyledProvider } from 'aliens-design-system-front';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      keepPreviousData: true,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <StyledProvider>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StyledProvider>,
 );
