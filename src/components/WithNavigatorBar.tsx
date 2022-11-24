3f232e650c875f90849557cb9c61f1c4a6d302bc
강석현 <tjrgus0713@gmail.com>
2022-11-24 19:56:42 +0900

    (#34) feat: 네비게이터 추가

--- src/components/WithNavigatorBar.tsx
+++ src/components/WithNavigatorBar.tsx
@@ -1,6 +1,7 @@
 import { NavigatorBar } from 'aliens-design-system-front';
 import styled from 'styled-components';
 import { ReactNode } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 const feature = {
   point_service: true,
@@ -12,14 +13,19 @@ const feature = {
   meal_service: false,
 };
 
-interface Props {
+interface PropsType {
   children: ReactNode;
 }
 
-export const WithNavigatorBar = ({ children }: Props) => {
+export const WithNavigatorBar = ({ children }: PropsType) => {
+  const { pathname } = useLocation();
   return (
     <_Flex>
-      <NavigatorBar features={feature} />
+      <NavigatorBar
+        Link={Link}
+        navList={['홈', '신청', '공지', '설문', '분실문', '마이페이지']}
+        pathname={pathname}
+      />
       {children}
     </_Flex>
   );
