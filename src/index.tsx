159d9cfe8f39dcdb6676ce9f061bc6a3b6e86a27
강석현 <tjrgus0713@gmail.com>
2022-11-29 20:01:28 +0900

    (#32) feat: 모달상태 전역으로 관리

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { App } from './App';
+import ModalProvider from '@/context/modal';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,9 +18,11 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <StyledProvider>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <ModalProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ModalProvider>
   </StyledProvider>,
 );
