53eb19dad9b30c1cbc286e17090eadad3471f0b4
강석현 <tjrgus0713@gmail.com>
2022-11-29 11:20:13 +0900

    (#31) feat: 4차 스프린트 퍼블리싱

--- src/components/WithNavigatorBar.tsx
+++ src/components/WithNavigatorBar.tsx
@@ -1,31 +1,26 @@
 import { NavigatorBar } from 'aliens-design-system-front';
 import styled from 'styled-components';
 import { ReactNode } from 'react';
-
-const feature = {
-  point_service: true,
-  apply_service: true,
-  notice_service: true,
-  survey_service: true,
-  lost_service: true,
-  my_page: true,
-  meal_service: false,
-};
+import { Link, useLocation } from 'react-router-dom';
 
 interface Props {
   children: ReactNode;
 }
 
-export const WithNavigatorBar = ({ children }: Props) => {
+export function WithNavigatorBar({ children }: Props) {
+  const { pathname } = useLocation();
   return (
     <_Flex>
-      <NavigatorBar features={feature} />
+      <NavigatorBar
+        Link={Link}
+        navList={['홈', '신청', '공지', '설문', '분실문', '마이페이지']}
+        pathname={pathname}
+      />
       {children}
     </_Flex>
   );
-};
+}
 
 const _Flex = styled.div`
   display: flex;
-  overflow: hidden;
 `;
