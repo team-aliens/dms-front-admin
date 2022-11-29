df34a5e12961c52c9af1114a7d1f21bb1e151b17
강석현 <tjrgus0713@gmail.com>
2022-11-29 20:04:39 +0900

    (#32) fix: 사소한 수정

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { App } from './App';
-import ModalProvider from '@/context/modal';
+import { ModalProvider } from '@/context/modal';
 
 const queryClient = new QueryClient({
   defaultOptions: {
