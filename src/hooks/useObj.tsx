1b0a39689308054fe5123beb832eae8bcfb4b0a1
강석현 <tjrgus0713@gmail.com>
2022-12-02 11:40:12 +0900

    (#36) delete : 필요없는 코드 삭제

--- src/hooks/useObj.tsx
+++ src/hooks/useObj.tsx
@@ -1,18 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export const useObj = <T extends string>(errorTypes: readonly T[]) => {
   type ObjState = {
     [keys in typeof errorTypes[number]]: string;
   };
   const [obj, setObj] = useState<ObjState>();
-  useEffect(() => {
-    errorTypes.forEach((i) => {
-      setObj({
-        ...obj,
-        [i]: '',
-      });
-    });
-  }, [errorTypes, obj]);
   const changeObjectValue = (errorType: keyof ObjState, message: string) => {
     setObj({
       ...obj,
