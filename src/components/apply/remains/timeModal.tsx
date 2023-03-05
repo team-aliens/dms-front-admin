90f0f215ac4603087f4e3e8e43edc803c5d5d974
jisoung <jangjisoung0405@gmail.com>
2023-03-06 00:31:50 +0900

    Fix : input -> dropdown으로 변경

--- src/components/apply/remains/timeModal.tsx
+++ src/components/apply/remains/timeModal.tsx
@@ -19,9 +19,19 @@ interface ITimeState {
   endHour: string;
   endMin: string;
 }
+
+const hourToArray = Array(24)
+  .fill(void 0)
+  .map((_, idx) => `${idx < 10 ? '0' + String(idx) : String(idx)}`);
+
+const minToArray = Array(60)
+  .fill(void 0)
+  .map((_, idx) => `${idx < 10 ? '0' + String(idx) : String(idx)}`);
+
+const dayToArray = ['월', '화', '수', '목', '금', '토', '일'];
 export default function TimeModal() {
   const { data: remainTime } = useGetRemainTime();
-  const { onHandleChange, state: timeState } = useForm<ITimeState>({
+  const { state: timeState, setState: setTimeState } = useForm<ITimeState>({
     startDay: getTextWithDay(remainTime?.start_day_of_week),
     startHour: remainTime?.start_time.slice(0, 2),
     startMin: remainTime?.start_time.slice(3, 5),
@@ -36,55 +46,80 @@ export default function TimeModal() {
     end_day_of_week: getDayWithText(timeState.endDay),
     end_time: `${timeState.endHour}:${timeState.endMin}:00`,
   });
+  useEffect(() => {
+    setTimeState({
+      startDay: getTextWithDay(remainTime?.start_day_of_week),
+      startHour: remainTime?.start_time.slice(0, 2),
+      startMin: remainTime?.start_time.slice(3, 5),
+      endDay: getTextWithDay(remainTime?.end_day_of_week),
+      endHour: remainTime?.end_time.slice(0, 2),
+      endMin: remainTime?.end_time.slice(3, 5),
+    });
+  }, [remainTime]);
   const onClick = () => {
     mutate();
     closeModal();
   };
+
+  const onChangeDropDown = (value: string, key: string) => {
+    setTimeState((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+
+  const onHandleChange = () => {};
   return (
     <Modal
       title="잔류 신청 시간 설정"
       inputList={[
         <_TimeWrapper key={'time'}>
-          <Input
-            onChange={onHandleChange}
-            name={'startDay'}
+          <DropDown
+            items={dayToArray}
+            placeholder={'요일'}
+            onChange={(value) => onChangeDropDown(value, 'startDay')}
             value={timeState.startDay}
-            key={'startDay'}
+            width={50}
           />
           <p className="day">요일</p>
-          <Input
-            onChange={onHandleChange}
-            name={'startHour'}
+          <DropDown
+            items={hourToArray}
+            placeholder={'시간'}
+            onChange={(value) => onChangeDropDown(value, 'startHour')}
             value={timeState.startHour}
-            key={'startHour'}
+            width={50}
           />
           <p className="day">:</p>
-          <Input
-            onChange={onHandleChange}
-            name={'startMin'}
+          <DropDown
+            items={minToArray}
+            placeholder={'분'}
+            onChange={(value) => onChangeDropDown(value, 'startMin')}
             value={timeState.startMin}
-            key={'startMin'}
+            width={50}
           />
           <p className="to">~</p>
-          <Input
-            onChange={onHandleChange}
-            name={'endDay'}
+          <DropDown
+            items={dayToArray}
+            placeholder={'요일'}
+            onChange={(value) => onChangeDropDown(value, 'endDay')}
             value={timeState.endDay}
-            key={'endDay'}
+            width={50}
           />
           <p className="day">요일</p>
-          <Input
-            onChange={onHandleChange}
-            name={'endHour'}
+          <DropDown
+            items={hourToArray}
+            placeholder={'시간'}
+            onChange={(value) => onChangeDropDown(value, 'endHour')}
             value={timeState.endHour}
-            key={'endHour'}
+            width={50}
           />
           <p className="day">:</p>
-          <Input
-            onChange={onHandleChange}
-            name={'endMin'}
+          <DropDown
+            items={minToArray}
+            placeholder={'분'}
+            onChange={(value) => onChangeDropDown(value, 'endMin')}
             value={timeState.endMin}
-            key={'endMin'}
+            width={50}
           />
         </_TimeWrapper>,
       ]}
