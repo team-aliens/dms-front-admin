7fb12ccb4e3861f9c8efbb746a3c739911dd7255
paperdeer <tjrgus0713@gmail.com>
2023-03-20 12:00:53 +0900

    chore: cypress ci project id 추가

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
   },
+  projectId: 'dms-front-admin',
   env: {
     'cypress-react-selector': {
       root: '#root',
