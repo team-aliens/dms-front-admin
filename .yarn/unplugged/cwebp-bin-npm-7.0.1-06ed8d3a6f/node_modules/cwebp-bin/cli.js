679fa505f2f25b5a52a02ea13c3e6042f09c6902
Josanghyeon <BStkdgus.dev@gmail.com>
2023-04-18 10:55:42 +0900

    (#138) refactor: yarn berry관련 파일 모두 삭제

--- .yarn/unplugged/cwebp-bin-npm-7.0.1-06ed8d3a6f/node_modules/cwebp-bin/cli.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import process from 'node:process';
-import {spawn} from 'node:child_process';
-import binPath from './index.js';
-
-spawn(binPath, process.argv.slice(2), {stdio: 'inherit'})
-	.on('exit', process.exit);
